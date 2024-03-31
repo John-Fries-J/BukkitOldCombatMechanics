b8a653914b8b7777df6f7c6086556fee528015dc
John-Fries-J <w.bababooie@gmail.com>
2024-03-31 18:31:13 +0100

    Remove all lapis things

--- src/main/java/kernitus/plugin/OldCombatMechanics/OCMMain.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/OCMMain.java
@@ -232,7 +232,7 @@ public class OCMMain extends JavaPlugin {
         ModuleLoader.addModule(new ModuleDisableProjectileRandomness(this));
         ModuleLoader.addModule(new ModuleDisableBowBoost(this));
         ModuleLoader.addModule(new ModuleProjectileKnockback(this));
-        ModuleLoader.addModule(new ModuleNoLapisEnchantments(this));
+//        ModuleLoader.addModule(new ModuleNoLapisEnchantments(this));
         ModuleLoader.addModule(new ModuleDisableEnderpearlCooldown(this));
         ModuleLoader.addModule(new ModuleChorusFruit(this));
 
